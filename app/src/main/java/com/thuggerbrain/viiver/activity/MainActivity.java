10a7b7086f4cdb3bb867e7271f983c9ca5a48d61
jakkavat <jakkavat@Jakkavats-MacBook-Pro.local>
2017-01-17 18:02:37 +0700

    Adding project sturcture and design

--- app/src/main/java/com/thuggerbrain/viiver/activity/MainActivity.java
+++ app/src/main/java/com/thuggerbrain/viiver/activity/MainActivity.java
@@ -1,23 +1,89 @@
 package com.thuggerbrain.viiver.activity;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.thuggerbrain.viiver.R;
-import com.thuggerbrain.viiver.utils.ScreenUtils;
+import com.thuggerbrain.viiver.fragment.FragmentMain;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int ScreenWidth = ScreenUtils.getInstance().getScreenWidth();
-        int ScreenHeight = ScreenUtils.getInstance().getScreenHeight();
+        initInstances();
 
-        Toast.makeText(MainActivity.this,"Width = "+ScreenWidth+", Height = "+ScreenHeight,Toast.LENGTH_SHORT).show();
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer, FragmentMain.newInstance())
+                    .commit();
+        }
+
 
     }
+
+
+    public void initInstances(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+    }
+
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
---
+++ app/src/main/java/com/thuggerbrain/viiver/fragment/FragmentMain.java
@@ -0,0 +1,154 @@
+package com.thuggerbrain.viiver.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.inthecheesefactory.thecheeselibrary.view.SlidingTabLayout;
+import com.thuggerbrain.viiver.R;
+
+
+/**
+ * Created by jakkavat on 1/15/2017 AD.
+ */
+@SuppressWarnings("unused")
+public class FragmentMain extends Fragment {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private int[] tabIcons = {
+            R.drawable.ic_search_white_36dp,
+            R.drawable.ic_notifications_white_36dp,
+            R.drawable.ic_search_white_36dp,
+            R.drawable.ic_notifications_white_36dp
+    };
+
+    public FragmentMain() {
+        super();
+    }
+
+    @SuppressWarnings("unused")
+    public static FragmentMain newInstance() {
+        FragmentMain fragment = new FragmentMain();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init(savedInstanceState);
+
+        if (savedInstanceState != null)
+            onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        initInstances(rootView, savedInstanceState);
+        return rootView;
+    }
+
+    private void init(Bundle savedInstanceState) {
+        // Init Fragment level's variable(s) here
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void initInstances(View rootView, Bundle savedInstanceState) {
+        // Init 'View' instance(s) with rootView.findViewById here
+
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        viewPager.setAdapter(new FragmentStatePagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return FragmentProfile.newInstance();
+                    case 1:
+                        return FragmentFeed.newInstance();
+                    case 2:
+                        return FragmentContact.newInstance();
+                    case 3:
+                        return FragmentTracking.newInstance();
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position) {
+                    case 0:
+                        return "Profile";
+                    case 1:
+                        return "Feed";
+                    case 2:
+                        return "Contact";
+                    case 3:
+                        return "Tracking";
+                    default:
+                        return "";
+                }
+            }
+
+
+        });
+
+        tabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+
+    }
+
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    /*
+     * Save Instance State Here
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save Instance State here
+    }
+
+    /*
+     * Restore Instance State Here
+     */
+    @SuppressWarnings("UnusedParameters")
+    private void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore Instance State here
+    }
+
+}

ffc86bdbcd1ffbb2272c64fdf8e230c23530f2c7
jakkavat <jakkavat@Jakkavats-MacBook-Pro.local>
2017-01-26 22:19:44 +0700

    Delete import.

--- app/src/main/java/com/thuggerbrain/viiver/activity/OrderActivity.java
+++ app/src/main/java/com/thuggerbrain/viiver/activity/OrderActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.thuggerbrain.viiver.R;
-import com.thuggerbrain.viiver.fragment.FragmentMain;
 import com.thuggerbrain.viiver.fragment.FragmentOrder;
 
 public class OrderActivity extends AppCompatActivity {

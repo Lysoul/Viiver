ed9685d147772cf157323f150bfed0848e999d1a
Keroaz <nongkobnar@gmail.com>
2017-02-02 15:34:53 +0700

    Upadte New to View

--- app/src/main/java/com/thuggerbrain/viiver/fragment/Fragment_ContactDialog.java
+++ app/src/main/java/com/thuggerbrain/viiver/fragment/Fragment_ContactDialog.java
@@ -18,7 +18,6 @@ public class Fragment_ContactDialog extends android.support.v4.app.DialogFragmen
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_dialogcontact,container,false);
         return rootView;
     }
